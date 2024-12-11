e13694548834d2b5d05d7c466a91401527f2e5ca
senda1988 <zidi.senda@gmail.com>
2024-12-11 14:54:14 +0100

    Update Math.py

--- Math.py
+++ Math.py
@@ -1,4 +1,4 @@
-
+# Einfache Mathematik
 a = 7
 b = 5
 
@@ -10,13 +10,23 @@ print("Multiplikation: ", a * b)
 
 print("Division: ", a / b)
 
+#Durchschnitt berechnen
 note1 = 15
-
 note2 = 16
-
 note3 = 20
-
-
 l = [note1, note3, note3]
 s = sum(l)/3
 print("Mein Durchschnitt ist: ", s)
+
+
+# Kilometer in Meilen umrechnen
+# Umrechnungsfaktor: 1 Kilometer = 0.621371 Meilen
+f = 0.621371
+
+# Den Nutzer nach der Kilometeranzahl fragen
+km = int(input("Wie viele Kilometer möchtest du umrechnen? "))
+
+# Umrechnung in Meilen
+meilen = km * f
+
+print("Umrechnung in Meilen: ",  km * f)
